99b3e1fe94a559fe3ad627fc8aa092b02067d341
gabdura <gadzhi.abdurazakov@daimler.com>
2022-01-19 17:38:16 +0100

    added docker build to start script

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
+docker build -t spotify .
 docker container rm spotify
 docker run --network=host --device /dev/snd:/dev/snd --group-add audio -e SPOTIFY_USERNAME=$SPOTIFY_USERNAME -e SPOTIFY_PASSWORD=$SPOTIFY_PASSWORD --name spotify -d spotify
