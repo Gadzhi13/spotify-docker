570845a8f20d08fb874bf77076d1d8e3acc6785e
gabdura <gadzhi.abdurazakov@daimler.com>
2022-01-19 16:45:13 +0100

    tmp

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
+docker container rm spotify
 docker run --network=host --device /dev/snd:/dev/snd --group-add audio -e SPOTIFY_USERNAME=$SPOTIFY_USERNAME -e SPOTIFY_PASSWORD=$SPOTIFY_PASSWORD --name spotify spotify
