5deb5702f66e7bdf65c151750f48ef502be8d8b7
gabdura <gadzhi.abdurazakov@daimler.com>
2022-01-28 17:00:40 +0100

    added if

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 var express = require('express')
 var app = express()
-const { exec } = require('child_process');
+const { exec } = require('child_process')
 
 var restart_spotify = exec('sh /home/gadzhi/projects/spotify-docker/start.sh',
         (error, stdout, stderr) => {
-            console.log(stdout);
-            console.log(stderr);
+            console.log(stdout)
+            console.log(stderr)
             if (error !== null) {
-                console.log(`exec error: ${error}`);
+                console.log(`exec error: ${error}`)
             }
-        });
+        })
 
 app.post('/restart_spotify', function(request, response) {
   console.log('POST /restart_spotify')
-  restart_spotify();
+  if (request.body == "restart") {
+    restart_spotify()
+  }
   response.writeHead(200, {'Content-Type': 'text/html'})
   response.end('Spotify restarted')
 })
