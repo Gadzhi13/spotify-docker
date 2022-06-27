5ef71e2182cea989723a83797e37a54a724283f6
Abdurazakov Gadzhi <abdurazakov.gadzhi@gmail.com>
2022-06-27 08:49:50 +0000

    spotify restarter servicerized

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ docker container stop spotify
 docker container rm spotify
 docker run --network=host --device /dev/snd:/dev/snd --group-add audio -e SPOTIFY_USERNAME=$SPOTIFY_USERNAME -e SPOTIFY_PASSWORD=$SPOTIFY_PASSWORD --name spotify -d --restart unless-stopped --privileged spotify
 
-forever start "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/server.js 3333
+#forever start "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/server.js 3333
